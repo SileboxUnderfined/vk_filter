bff96f0bf66cc058d2893be58d8846fe5267ebb5
GregoryAtom22 <62352393+GregoryAtom22@users.noreply.github.com>
2020-05-10 19:42:02 +0300

    Add files via upload
    popup посмотрите как вам

---
+++ files/Alexandra/popup.js
@@ -0,0 +1,10 @@
+//let changeColor = document.getElementById('changeColor');
+//
+//chrome.storage.sync.get('color', function(data) {
+//	changeColor.style.backgroundColor = data.color;
+//	changeColor.setAttribute('value', data.color);
+//});
+document.getElementById("create-file").addEventListener("click", function(){
+	let formelement = document.getElementById("getkeywords");
+	formelement.style.display = "block";
+});
